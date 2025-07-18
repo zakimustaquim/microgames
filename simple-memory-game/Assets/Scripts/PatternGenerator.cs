b9c9b7a743b6b717fbaa425b0953c129b7d12819
zakimustaquim <104472530+zakimustaquim@users.noreply.github.com>
2025-07-17 23:05:41 -0400

    Quadrant script skeleton

--- simple-memory-game/Assets/Scripts/PatternGenerator.cs
+++ simple-memory-game/Assets/Scripts/PatternGenerator.cs
@@ -4,10 +4,12 @@ using System.Collections.Generic;
 public class PatternGenerator
 {
     private int currLength;
+    private readonly int numCells;
 
-    public PatternGenerator()
+    public PatternGenerator(int numCells)
     {
         currLength = 3;
+        this.numCells = numCells;
     }
 
     public IList<GameEvent> GenerateNext()
@@ -17,7 +19,10 @@ public class PatternGenerator
 
         for (int i = 0; i < currLength; i++)
         {
-            pattern.Add(new GameEvent((Colors)random.Next(0, 4), random.Next(0, 4)));
+            pattern.Add(new GameEvent(
+                (Colors)random.Next(0, Enum.GetValues(typeof(Colors)).Length),
+                random.Next(0, numCells)
+            ));
         }
 
         currLength++;
---
+++ simple-memory-game/Assets/Scripts/QuadrantScript.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuadrantScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        SetOpacity(0f); // Set initial opacity to 0
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SetOpacity(float opacity)
+    {
+        Color color = gameObject.GetComponent<Image>().color;
+        color.a = opacity; // Set the opacity
+        SetColor(color); // Apply the color with the new opacity
+    }
+    
+    void SetColor(Color color)
+    {
+        gameObject.GetComponent<Image>().color = color; // Set the color
+    }
+}
