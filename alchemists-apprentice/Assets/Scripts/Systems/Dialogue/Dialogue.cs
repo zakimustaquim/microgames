b951ad2d2348c2e90b9a9e16f095a252e255ac3c
Zaki Mustaquim <mustaquim.zaki@gmail.com>
2025-10-16 15:25:06 -0400

    Add create asset menu declaration

--- alchemists-apprentice/Assets/Scripts/Systems/Dialogue/Dialogue.cs
+++ alchemists-apprentice/Assets/Scripts/Systems/Dialogue/Dialogue.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 /// In the event that multiple dialogues are needed for a single
 /// interactable based on game state, it may have multiple dialogues.
 /// </summary>
+[CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue/Dialogue", order = 0)]
 public class Dialogue : LoggingScriptableObject
 {
     public string dialogueID;
